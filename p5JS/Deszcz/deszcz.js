849bd609c47ede9a963016b8d3f8c03938415f79
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-11-29 15:51:30 +0100

    Test dzwieku

--- p5JS/Deszcz/deszcz.js
+++ p5JS/Deszcz/deszcz.js
@@ -1,7 +1,13 @@
 var krople = []
+var song;
+
+function preload() {
+	song = loadSound('assets/rain.mp3');
+}
 
 function setup() {
 	createCanvas(640,360);
+	song.play();
 	for (var i = 0; i < 500; i++) {
 		krople[i] = new Kropla();
 	}
