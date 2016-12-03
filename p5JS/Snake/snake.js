bcb68d59245c075cfe167640011d76a65ea59cdd
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-03 14:24:30 +0100

    jedzonko!

--- p5JS/Snake/snake.js
+++ p5JS/Snake/snake.js
@@ -1,35 +1,53 @@
 var snake;
 var img;
 var song;
-var skala = 27;
+var ding;
+var skala = 20;
+
+var jedzonko;
 
 function preload() {
-  img = loadImage('images/tetris.png');
-  song = loadSound('assets/mario.mp3');
+	img = loadImage('images/tetris.png');
+	song = loadSound('assets/mario.mp3');
+	ding = loadSound('assets/ding.mp3');
 }
 
 function setup() {
-  createCanvas(600, 600);
-  song.play();
-  s = new Waz();
-  frameRate(10);
+	createCanvas(600, 600);
+	song.play();
+	s = new Waz();
+	frameRate(10);
+	lokacja();
 }
 
+function lokacja(){
+	var wiersze = floor(width/skala);
+	var kolumny = floor(height/skala);
+	jedzonko = createVector(floor(random(kolumny)), floor(random(wiersze)));
+}
 
 function draw() {
-  image(img, 0, 0);
-  s.update();
-  s.show();
+	image(img, 0, 0);
+	s.update();
+	s.show();
+
+	if(s.zjedz(jedzonko)) {
+		lokacja();
+		ding.play();
+	}
+
+	fill(255,0,100);
+	rect(jedzonko.x, jedzonko.y, skala, skala);
 }
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.kierunek(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.kierunek(0,1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.kierunek(1,0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.kierunek(-1,0);
-  }
+	if (keyCode === UP_ARROW) {
+		s.kierunek(0, -1);
+	} else if (keyCode === DOWN_ARROW) {
+		s.kierunek(0,1);
+	} else if (keyCode === RIGHT_ARROW) {
+		s.kierunek(1,0);
+	} else if (keyCode === LEFT_ARROW) {
+		s.kierunek(-1,0);
+	}
 }
--- p5JS/Snake/waz.js
+++ p5JS/Snake/waz.js
@@ -5,20 +5,29 @@ function Waz() {
   this.szybkoscY = 0;
 
   this.kierunek = function(x, y){
-  	this.szybkoscX = x;
-  	this.szybkoscY = y;
+	this.szybkoscX = x;
+	this.szybkoscY = y;
   }
 
   this.update = function() {
 	this.x = this.x + this.szybkoscX * skala;
 	this.y = this.y + this.szybkoscY * skala;
 
-	this.x = constrain(this.x, 0 ,width - skala);
-	this.y = constrain(this.y, 0, height - skala);
+	this.x = constrain(this.x, 1 ,width - skala);
+	this.y = constrain(this.y, 1, height - skala);
   }
 
   this.show = function() {
-    fill(255);
-    rect(this.x, this.y, skala, skala);
+	fill(255);
+	rect(this.x, this.y, skala, skala);
+  }
+
+  this.zjedz = function(pos) {
+  	var d = dist(this.x, this.y, pos.x, pos.y);
+  	if (d<1){
+  		return true;
+  	}else{
+  		return false;
+  	}
   }
 }
