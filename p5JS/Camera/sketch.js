a13f057a023d85f5f11cb4dbb32ee237c81fb0d6
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-15 19:34:11 +0100

    Optymalizacji czas dalszy

--- p5JS/Camera/sketch.js
+++ p5JS/Camera/sketch.js
@@ -4,7 +4,7 @@ var vScale = 16;
 
 function setup() {
   createCanvas(320, 240);
-  //pixelDensity(10);
+  pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(320,240);
   //video.size(width/vScale, height/vScale);
@@ -12,27 +12,21 @@ function setup() {
 
 function draw() {
   background(51);
-  image(video, 0, 0, 320, 240);
-  filter(DILATE);
-  //video.loadPixels();
-  //loadPixels();
-  //for (var y = 0; y < video.height; y++) {
-  //  for (var x = 0; x < video.width; x++) {
-  //    var index = (video.width - x + 1 + (y * video.width))*4;
-  //    var r = video.pixels[index+0];
-  //    var g = video.pixels[index+1];
-  //    var b = video.pixels[index+2];
-  //
-  //    var bright = (r+g+b)/3;
-  //
-  //    var w = map(bright, 0, 255, 0, vScale);
-  //
-  //    noStroke();
-  //    fill(255);
-  //    rectMode(CENTER);
-  //    rect(x*vScale, y*vScale, w, w);
-  //
-  //  }
-  //}
+  //image(video, 0, 0, 320, 240);
+  //filter(DILATE);
+  video.loadPixels();
+  loadPixels();
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (x + y * video.width)*4;
+      var r = video.pixels[index+0];
+      var g = video.pixels[index+1];
+      var b = video.pixels[index+2];
+  
+      var bright = (r+g+b)/3;
+      fill(bright);
+      rect(x*vSacale,y*vSacale,vSacale,vScale);
+    }
+  }
  
 }
