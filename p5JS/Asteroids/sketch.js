f3bb8ef3134f4de3049f7d2869b6dde9b732561a
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-15 13:05:03 +0100

    Kamerka!

--- p5JS/Asteroids/sketch.js
+++ p5JS/Asteroids/sketch.js
@@ -3,10 +3,16 @@ var asteroids = [];
 var lasers = [];
 var score = 0;
 
+var video;
+var vScale = 16;
+
 function setup() {
 	console.log(score);
 	//createCanvas(windowWidth, windowHeight);
 	createCanvas(640,360);
+	pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(width/vScale, height/vScale);
 	ship = new Ship();
 	for (var i = 0; i < 5; i++) {
 		asteroids.push(new Asteroid());
@@ -15,6 +21,26 @@ function setup() {
 
 function draw() {
 	background(0);
+	video.loadPixels();
+  loadPixels();
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width))*4;
+      var r = video.pixels[index+0];
+      var g = video.pixels[index+1];
+      var b = video.pixels[index+2];
+
+      var bright = (r+g+b)/3;
+
+      var w = map(bright, 0, 255, 0, vScale);
+
+      noStroke();
+      fill(255);
+      rectMode(CENTER);
+      rect(x*vScale, y*vScale, w, w);
+
+    }
+  }
 	textSize(32);
 	textFont("Helvetica");
 	var pom1 = random(10, 255);
