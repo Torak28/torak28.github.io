4e1c1dc786dcd4584d5fc0c5bd5d79d41b926e07
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-15 19:45:00 +0100

    nic wiecej nie zrobie na serwerze :c

--- p5JS/Camera/sketch.js
+++ p5JS/Camera/sketch.js
@@ -1,32 +1,15 @@
 var video;
 
-var vScale = 12;
-
 function setup() {
-  createCanvas(160, 120);
+  createCanvas(320, 240);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(160,120);
-  //video.size(width/vScale, height/vScale);
+  video.size(320,240);
 }
 
 function draw() {
   background(255);
-  //image(video, 0, 0, 320, 240);
-  //filter(DILATE);
-  video.loadPixels();
-  loadPixels();
-  for (var y = 0; y < video.height; y++) {
-    for (var x = 0; x < video.width; x++) {
-      var index = (x + y * video.width)*4;
-      var r = video.pixels[index+0];
-      var g = video.pixels[index+1];
-      var b = video.pixels[index+2];
-  
-      var bright = (r+g+b)/3;
-      fill(bright);
-      rect(x*vScale,y*vScale,vScale,vScale);
-    }
-  }
+  image(video, 0, 0, 320, 240);
+  filter(DILATE);
  
 }
