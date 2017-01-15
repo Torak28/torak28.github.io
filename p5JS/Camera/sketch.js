c289b74e2c01acee42c7181a0cbe4841f2c25e1e
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-15 19:25:18 +0100

    dzialaj

--- p5JS/Camera/sketch.js
+++ p5JS/Camera/sketch.js
@@ -4,7 +4,7 @@ var vScale = 16;
 
 function setup() {
   createCanvas(320, 240);
-  pixelDensity(10);
+  //pixelDensity(10);
   video = createCapture(VIDEO);
   video.size(320,240);
   //video.size(width/vScale, height/vScale);
@@ -12,26 +12,26 @@ function setup() {
 
 function draw() {
   background(51);
-
-  video.loadPixels();
-  loadPixels();
-  for (var y = 0; y < video.height; y++) {
-    for (var x = 0; x < video.width; x++) {
-      var index = (video.width - x + 1 + (y * video.width))*4;
-      var r = video.pixels[index+0];
-      var g = video.pixels[index+1];
-      var b = video.pixels[index+2];
-
-      var bright = (r+g+b)/3;
-
-      var w = map(bright, 0, 255, 0, vScale);
-
-      noStroke();
-      fill(255);
-      rectMode(CENTER);
-      rect(x*vScale, y*vScale, w, w);
-
-    }
-  }
+  image(capture, 0, 0, 320, 240);
+  //video.loadPixels();
+  //loadPixels();
+  //for (var y = 0; y < video.height; y++) {
+  //  for (var x = 0; x < video.width; x++) {
+  //    var index = (video.width - x + 1 + (y * video.width))*4;
+  //    var r = video.pixels[index+0];
+  //    var g = video.pixels[index+1];
+  //    var b = video.pixels[index+2];
+  //
+  //    var bright = (r+g+b)/3;
+  //
+  //    var w = map(bright, 0, 255, 0, vScale);
+  //
+  //    noStroke();
+  //    fill(255);
+  //    rectMode(CENTER);
+  //    rect(x*vScale, y*vScale, w, w);
+  //
+  //  }
+  //}
  
 }
