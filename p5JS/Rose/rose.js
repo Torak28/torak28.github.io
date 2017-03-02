33a40d9350dbdaead29e7a24b5178d2e8b9d7bcb
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-03-02 16:45:06 +0100

    Roza poczatek 2

--- p5JS/Rose/rose.js
+++ p5JS/Rose/rose.js
@@ -1,7 +1,21 @@
+var k = 5/8;
 function setup(){
-
+	createCanvas(400,400);
 }
 
 function draw(){
-	
+	background(51);
+	translate(width/2, height/2);
+
+	beginShape();
+		stroke(255);
+		noFill();
+		strokeWeight(1);
+		for (var i = 0; i < TWO_PI * 10; i+=0.02) {
+			var r = 200 * cos(k * a);
+			var x = r * cos(a);
+			var y = r * sin(a);
+			vertex(x,y);
+		}
+	endShape(CLOSE);
 }
